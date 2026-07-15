5bbfdd42bcbd10a1167085a8ede49d307451d37b
Dejan Tomic <hi@iamdejan.com>
2026-07-15 19:08:23 +0200

    shared: providers and actions addons

---
+++ shared/load.js
@@ -0,0 +1,12 @@
+/* Addons */
+import './addons/providers/addon.js';
+import './addons/actions/addon.js';
+
+/* Providers */
+import './items/providers/slack.js';
+import './items/providers/github.js';
+import './items/providers/google.js';
+
+/* Actions */
+import './items/actions/slack/send.js';
+import './items/actions/github/issue.js';
